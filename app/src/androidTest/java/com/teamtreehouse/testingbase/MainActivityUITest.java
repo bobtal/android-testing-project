b79957e4727a97f8ef72ad95178836621566a79a
bobtal <talevski.boban@gmail.com>
2018-01-06 22:09:26 +0100

    end of Testing the Spinner video

--- app/src/androidTest/java/com/teamtreehouse/testingbase/MainActivityUITest.java
+++ app/src/androidTest/java/com/teamtreehouse/testingbase/MainActivityUITest.java
@@ -1,18 +1,29 @@
 package com.teamtreehouse.testingbase;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.test.espresso.matcher.BoundedMatcher;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.view.View;
+import android.widget.LinearLayout;
 
+import org.hamcrest.Description;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.core.AllOf.allOf;
 
 @RunWith(AndroidJUnit4.class)
 public class MainActivityUITest {
@@ -37,13 +48,33 @@ public class MainActivityUITest {
     @Test
     public void spinnerUpdatesBackgroundColor() throws Exception {
         // Arrange
-
+        final int givenColor = Color.GREEN;
+        String spinnerItemText = "Green";
 
         // Act
-
+        onView(withId(R.id.colorSpinner)).perform(click());
+//        onView(withText(spinnerItemText)).perform(click());
+        // above wouldn't work if the spinner has 100 items and we need to click on the
+        // 98th one because it isn't visible, which is why we should use onData instead of onView
+        // onView only searches through the view hierarchy
+        // onData searches through everything
+        onData(allOf(is(instanceOf(String.class)), is(spinnerItemText))).perform(click());
 
         // Assert
+        BoundedMatcher<View, LinearLayout> backgroundColorMatcher =
+                new BoundedMatcher<View, LinearLayout>(LinearLayout.class) {
+            @Override
+            protected boolean matchesSafely(LinearLayout linearLayout) {
+                int actualColor = ((ColorDrawable)linearLayout.getBackground()).getColor();
+                return givenColor == actualColor;
+            }
 
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("background color should equal: " + givenColor);
+            }
+        };
+        onView(withId(R.id.linearLayout)).check(matches(backgroundColorMatcher));
     }
 
     @Test
