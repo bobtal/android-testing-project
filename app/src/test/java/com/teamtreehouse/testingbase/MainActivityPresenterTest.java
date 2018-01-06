53fd330c92ec0fbdbfe16b35481248a469f4ef11
bobtal <talevski.boban@gmail.com>
2018-01-06 16:41:15 +0100

    end of Mocking with Mockito video

--- app/src/test/java/com/teamtreehouse/testingbase/MainActivityPresenterTest.java
+++ app/src/test/java/com/teamtreehouse/testingbase/MainActivityPresenterTest.java
@@ -1,37 +1,47 @@
 package com.teamtreehouse.testingbase;
 
+import android.graphics.Color;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MainActivityPresenterTest {
 
     MainActivityPresenter presenter;
+
+    @Mock
     MainActivityView view;
 
-    class MockedView implements MainActivityView {
-        String textViewText;
-
-        @Override
-        public void changeTextViewText(String text) {
-            textViewText = text;
-        }
-
-        @Override
-        public void changeBackgroundColor(int color) {
-
-        }
-
-        @Override
-        public void launchOtherActivity(Class activity) {
-
-        }
-    }
+    // without Mockito
+//    class MockedView implements MainActivityView {
+//        String textViewText;
+//
+//        @Override
+//        public void changeTextViewText(String text) {
+//            textViewText = text;
+//        }
+//
+//        @Override
+//        public void changeBackgroundColor(int color) {
+//
+//        }
+//
+//        @Override
+//        public void launchOtherActivity(Class activity) {
+//
+//        }
+//    }
 
     @Before
     public void setUp() throws Exception {
-        view = new MockedView();
+//        view = new MockedView(); without Mockito
         presenter = new MainActivityPresenter(view);
     }
 
@@ -44,15 +54,33 @@ public class MainActivityPresenterTest {
         presenter.editTextUpdated(givenString);
 
         // Assert
-        assertEquals(givenString, ((MockedView)view).textViewText);
+//        assertEquals(givenString, ((MockedView)view).textViewText); without Mockito
+        Mockito.verify(view).changeTextViewText(givenString);
     }
 
     @Test
     public void colorSelected() throws Exception {
+        // Arrange
+        int index = 2;
+        int givenColor = Color.GREEN;
+
+        // Act
+        presenter.colorSelected(index);
+
+        // Assert
+        Mockito.verify(view).changeBackgroundColor(givenColor);
     }
 
     @Test
     public void launchOtherActivityButtonClicked() throws Exception {
+        // Arrange
+        Class clazz = OtherActivity.class;
+
+        // Act
+        presenter.launchOtherActivityButtonClicked(clazz);
+
+        // Assert
+        Mockito.verify(view).launchOtherActivity(clazz);
     }
 
 }
